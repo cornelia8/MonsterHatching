d0200b3ebd4c4e2117bea6d6605e2b95ae305468
Cornelia Gabriela Marculescu <cornelia.gabriela98@gmail.com>
2023-04-18 16:32:45 +0300

    swipe right to pop up nav menu

--- app/src/main/java/com/example/monsterhatching/MainActivity.kt
+++ app/src/main/java/com/example/monsterhatching/MainActivity.kt
@@ -9,51 +9,38 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.monsterhatching.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // handle navigation menu
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.apply {
-            toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
-            drawerLayout.addDrawerListener(toggle)
-            toggle.syncState()
-
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-            navView.setNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.home -> { true }
-                    R.id.inventory -> { true }
-                    R.id.board -> { true }
-                    else -> false
-                }
-            }
-        }
-
-        fun onOptionsItemSelected(item: MenuItem): Boolean {
-            if (toggle.onOptionsItemSelected(item)) {
-                true
-            }
-            return super.onOptionsItemSelected(item)
-        }
-
-        // constraint layout stuff
         setContentView(R.layout.activity_main)
 
+        // handle navigation menu
+        drawerLayout = findViewById(R.id.drawerLayout_id)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // constraint layout stuff
         val startIntent = intent
         val username = startIntent.getStringExtra("username")
 
         val usernameTextView : TextView = findViewById(R.id.user_name)
         usernameTextView.text = String.format(getString(R.string.display_username), username)
+
+        val navigationView = findViewById<NavigationView>(R.id.navView)
+        val headerView = navigationView.getHeaderView(0)
+        val usernameNavTextView : TextView = headerView.findViewById<TextView>(R.id.nav_username)
+        usernameNavTextView.text = String.format(getString(R.string.display_username), username)
 
         // log out
         val logOutCue: ImageView = findViewById(R.id.logOut)
@@ -61,6 +48,11 @@ class MainActivity : AppCompatActivity() {
             val logOutIntent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(logOutIntent)
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
